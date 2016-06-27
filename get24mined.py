47256a7ddd6be38e3f38a56862ae86f4bb7fae08
ggs134 <inditow@gmail.com>
2016-06-27 11:28:13 +0900

    getHashAndREboot.py added

--- get24mined.py
+++ get24mined.py
@@ -1,8 +1,8 @@
 import requests
-import time
+# import time
 import datetime
 
-def getMinedEther(timeNow=time.gmtime()):
+def getMinedEther():
     res = requests.get("http://ethereum.miningpoolhub.com/index.php?page=api&action=getusertransactions&api_key=a8c9f5ea1a4045f6809c9a47c4746f5ae4aa5e136bf96ec0ce4223734c96a128")
     # res.json()
     data = res.json()['getusertransactions']['data']['transactions']
@@ -16,15 +16,20 @@ def getMinedEther(timeNow=time.gmtime()):
         # print 1,trTime
         # print 2,datetime.datetime.now()
         # print 3,datetime.timedelta(days=1)
+        print "TRTIME", trTime
+        print "UTC" ,datetime.datetime.utcnow()
+        print "diff", datetime.datetime.utcnow() - trTime > datetime.timedelta(days=1), datetime.datetime.utcnow() - trTime
         if transaction["type"] == "Fee":
             fee.append(data[i]['amount'])
         elif transaction["type"] == "Credit":
             credits.append(data[i]['amount'])
-        elif datetime.datetime.now() - trTime > datetime.timedelta(days=1):
+        elif datetime.datetime.utcnow() - trTime > datetime.timedelta(days=1):
+            # print datetime.datetime.utcnow()
             break
 
 
     netValue = sum(credits) - sum(fee)
+    print sum(credits)
     return netValue
     # print "credits", sum(credits)
     # print "fee", sum(fee)
---
+++ getHashAndReboot.py
@@ -0,0 +1,36 @@
+from threading import Thread
+import command_one
+import sys
+import time
+from datetime import datetime
+import requests
+
+class getMiningPoolHubData(Thread):
+    def __init__(self, interval):
+        super(getMiningPoolHubData,self).__init__()
+        self.interval = interval
+
+    def run(self):
+        while True:
+            response = requests.get("http://ethereum.miningpoolhub.com/index.php?page=api&action=getuserworkers&api_key=a8c9f5ea1a4045f6809c9a47c4746f5ae4aa5e136bf96ec0ce4223734c96a128")
+            json_response = response.json()
+            data = json_response["getuserworkers"]["data"]
+
+            for i in data:
+                miner_num = int(i["username"][13:])
+                if i["hashrate"] == 0:
+                    try:
+                        command_one.command_one(miner_num, "sudo reboot")
+                    except:
+                        sys.stderr.write(str(datetime.now())+" "+str(miner_num)+" Error Occured\n")
+                        pass
+                else:
+                     sys.stderr.write(str(datetime.now())+" miner"+str(miner_num)+" No problem\n")
+
+            # print data
+            # socketio.emit("miningpoolhub status", {"data": data}, namespace="/jsh")
+            time.sleep(self.interval)
+
+if __name__ == "__main__":
+    thread = getMiningPoolHubData(600)
+    thread.start()
